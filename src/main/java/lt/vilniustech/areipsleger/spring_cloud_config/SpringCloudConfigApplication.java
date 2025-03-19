2e1ee9833c316fba4e287eac52111fa32328da3b
Augustas Reipsleger <A53040@alunos.isel.pt>
2025-03-19 10:52:52 +0200

    first

---
+++ src/main/java/lt/vilniustech/areipsleger/spring_cloud_config/SpringCloudConfigApplication.java
@@ -0,0 +1,15 @@
+package lt.vilniustech.areipsleger.spring_cloud_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/vilniustech/areipsleger/spring_cloud_config/SpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vilniustech.areipsleger.spring_cloud_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
